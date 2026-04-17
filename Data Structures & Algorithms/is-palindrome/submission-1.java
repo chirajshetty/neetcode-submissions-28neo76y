0604bd9847b6e9e110914ffe0b44e3e1869f2854
chirajshetty <43310228+chirajshetty@users.noreply.github.com>
2026-04-17 08:35:02 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {     
+        char[] arr = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase().toCharArray();
+        int p1=0, p2 = arr.length - 1;
+        while(p1 <= p2){
+            // System.out.println(arr[p1] + " " + arr[p2] + " " + (arr[p1] == arr[p2]));
+            if(arr[p1] != arr[p2]) return false;
+            p1++;
+            p2--;
+        }
+
+        return true;
+    }
+}
