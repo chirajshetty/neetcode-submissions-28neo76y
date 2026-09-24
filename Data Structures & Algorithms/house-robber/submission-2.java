de7dd3c9172db53e1460e28f52dce56b4af25b9f
chirajshetty <43310228+chirajshetty@users.noreply.github.com>
2026-09-24 06:53:09 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        int next = 0;
+        int next1 = 0; 
+        int current = 0;
+
+        for(int i = nums.length - 1; i>= 0; i-- ){
+            current = Math.max(nums[i] + next1, next);
+            next1 = next;
+            next = current;
+        }
+
+        return current;
+    }
+}
